e6ffe72fbcdbcd7acbb8575d4f4391b60e171351
Author X5 <author@localhost.localdomain>
2019-01-12 19:11:27 +0700

    double table issue not solved yet

--- plc01.py
+++ plc01.py
@@ -4,6 +4,12 @@ from pymodbus.register_read_message import ReadInputRegistersResponse
 from pprint import pprint
 import time
 import linuxcnc, hal
+import json
+
+
+
+#BROKER PROGRAM PATH MULTI TABLE
+brokerProg = "brokerProg.json"
 
 #COM CONF
 COM_PORT = "/dev/ttyS0"
@@ -20,7 +26,7 @@ S_CNT = 70
 B1_OFFS = 20
 B1_CNT = 20
 B2_OFFS = 60
-B2_CNT = 20
+B2_CNT = 23
 
 plc01 = hal.component("plc01")
 #CNC <- PLC
@@ -79,12 +85,12 @@ plc01.newpin("S48", hal.HAL_BIT,hal.HAL_OUT) #DHOOD DOWN SENS POS
 plc01.newpin("S49", hal.HAL_BIT,hal.HAL_OUT) #DHOOD UP SENS POS
 plc01.newpin("S50", hal.HAL_BIT,hal.HAL_OUT) #TOOL DETECTION
 plc01.newpin("S51", hal.HAL_BIT,hal.HAL_OUT) #DUSTHOOD UP DETECTION
-plc01.newpin("S52", hal.HAL_BIT,hal.HAL_OUT) #
-plc01.newpin("S53", hal.HAL_BIT,hal.HAL_OUT) #
-plc01.newpin("S54", hal.HAL_BIT,hal.HAL_OUT) #
-plc01.newpin("S55", hal.HAL_BIT,hal.HAL_OUT) #
-plc01.newpin("S56", hal.HAL_BIT,hal.HAL_OUT) #
-plc01.newpin("S57", hal.HAL_BIT,hal.HAL_OUT) #
+plc01.newpin("S52", hal.HAL_BIT,hal.HAL_OUT) #CSTART A
+plc01.newpin("S53", hal.HAL_BIT,hal.HAL_OUT) #CSTART B
+plc01.newpin("S54", hal.HAL_BIT,hal.HAL_OUT) #CSTART C
+plc01.newpin("S55", hal.HAL_BIT,hal.HAL_OUT) #CSTART D
+plc01.newpin("S56", hal.HAL_BIT,hal.HAL_OUT) #MAG SENSOR POS
+plc01.newpin("S57", hal.HAL_BIT,hal.HAL_OUT) #MAG HOMED
 plc01.newpin("S58", hal.HAL_BIT,hal.HAL_OUT) #
 plc01.newpin("S59", hal.HAL_BIT,hal.HAL_OUT) #
 #STATUS OUTPUT
@@ -108,7 +114,6 @@ plc01.newpin("S76", hal.HAL_BIT,hal.HAL_IN) #REFER TO OUTPUT BIT
 plc01.newpin("S77", hal.HAL_BIT,hal.HAL_IN) #REFER TO OUTPUT BIT
 plc01.newpin("S78", hal.HAL_BIT,hal.HAL_IN) #REFER TO OUTPUT BIT
 plc01.newpin("S79", hal.HAL_BIT,hal.HAL_IN) #REFER TO OUTPUT BIT
-
 
 #CNC -> PLC
 plc01.newpin("B20", hal.HAL_BIT,hal.HAL_IN) #MACHINE RST
@@ -154,6 +159,9 @@ plc01.newpin("B77", hal.HAL_BIT,hal.HAL_IN) #BAR CD SETUP
 plc01.newpin("B78", hal.HAL_BIT,hal.HAL_IN) #SON-C
 plc01.newpin("B79", hal.HAL_BIT,hal.HAL_IN) #SON-X
 
+plc01.newpin("B80", hal.HAL_BIT,hal.HAL_IN) #LOCK TABLE AB
+plc01.newpin("B81", hal.HAL_BIT,hal.HAL_IN) #LOCK TABLE CD
+plc01.newpin("B82", hal.HAL_BIT,hal.HAL_IN) #HOME MAG
 
 #DATA CNC->PLC
 plc01.newpin("D201", hal.HAL_U32,hal.HAL_IN) #MAG INDEX COM
@@ -163,16 +171,129 @@ plc01.newpin("D201F", hal.HAL_FLOAT,hal.HAL_IN) #MAG INDEX COM
 plc01.newpin("E200F", hal.HAL_FLOAT,hal.HAL_OUT) #MAG INDEX COM
 plc01.newpin("E201F", hal.HAL_FLOAT,hal.HAL_OUT) #MAG INDEX COM
 
+#internal signal
+plc01.newpin("cstart", hal.HAL_BIT,hal.HAL_OUT) #CYCLE START
+plc01.newpin("program-is-run", hal.HAL_BIT,hal.HAL_IN) #PROGRAM RUN FLAGS
 
+#alarmCode
+plc01.newpin("E01", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E02", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E03", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E04", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E05", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E06", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E07", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E08", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E09", hal.HAL_BIT,hal.HAL_OUT)
+plc01.newpin("E010", hal.HAL_BIT,hal.HAL_OUT)
 
 client= ModbusClient(method = COM_METHOD, port=COM_PORT, stopbits = COM_STOPBITS, bytesize = COM_BSIZE, parity = COM_PARITY, baudrate= COM_BAUD)
 connection = client.connect()
-
 
 time.sleep(2)
 plc01.ready()
 regs = client.read_holding_registers(4297, 1, unit=1)
 plc01['D201'] = regs.registers[0]
+
+class table:
+
+	#DATUM T1
+	datum_x1 = 533.917
+	datum_y1 = -790.665
+
+	#DATUM T2
+	datum_x2 = 533.917 + 2350.
+	datum_y2 = -790.665
+
+	def __init__(self):	
+		self.ab_state = 0
+		self.cd_state = 0
+		self.s = linuxcnc.stat()
+		self.c = linuxcnc.command()
+		self.queue = [] #means no job at all
+		self.currJob = 0
+		self.lastRun = False
+	def jsonReader(self,filename=brokerProg):
+		data = None
+		with open(filename,"r") as f_read:
+			data = json.load(f_read)
+			
+		if data is None:
+			return None
+	
+		return data
+	
+	def prepare_ab(self):
+		if (plc01['S16'] is True) and (self.ab_state is 0):
+			if((plc01['S52'] is True) or (plc01['S53'] is True)):
+				param = self.jsonReader()
+				self.queue.append(['t1',param['table_1_info']['filename']])
+				self.ab_state = 1		 
+	
+	def prepare_cd(self):
+		if (plc01['S17'] is True) and (self.cd_state is 0):
+			if((plc01['S54'] is True) or (plc01['S55'] is True)):
+				param = self.jsonReader()
+				self.queue.append(['t2',param['table_2_info']['filename']])
+				self.cd_state = 1		 
+		
+	def double_table(self, runFlag):
+		#SCAN TABLE 1
+		if (plc01['B31'] is True):
+			self.prepare_ab()
+		#SCAN TABLE 2	
+		if (plc01['B32'] is True):
+			self.prepare_cd()
+		
+
+		if len(self.queue) != 0 and runFlag is False and self.currJob == 0:
+			prog = self.queue[0]
+			print("load Program :")
+			print(prog[1])
+			print("Table :")
+			print(prog[0])
+
+			self.c.program_open(prog[1])
+			self.c.reset_interpreter()
+			
+			if prog[0] == "t1":
+				mdi_str = "G10 L2 P1 X" + str(self.datum_x1) + " Y" + str(self.datum_y1)
+				print(mdi_str)
+				self.c.mdi(mdi_str)
+				self.c.wait_complete()
+				self.c.program_open(prog[1])
+				self.c.reset_interpreter()
+				self.c.mode(linuxcnc.MODE_AUTO)
+				self.c.wait_complete()
+				self.c.auto(linuxcnc.AUTO_RUN,0)
+				self.ab_state=2
+				self.currJob=1
+			if prog[0] == "t2":
+				mdi_str = "G10 L2 P1 X" + str(self.datum_x2) + " Y" + str(self.datum_y2)
+				print(mdi_str)
+				self.c.mdi(mdi_str)
+				self.c.mode(linuxcnc.MODE_AUTO)
+				self.c.program_open(prog[1])
+				self.c.reset_interpreter()
+				self.c.auto(linuxcnc.AUTO_RUN,0)
+				self.cd_state=2 
+				self.currJob=2				
+			self.queue.remove(prog)
+			return True,self.currJob 
+
+		if self.lastRun is True and runFlag is False:
+			if self.ab_state==2 and self.currJob==1:
+				self.ab_state=0
+				self.currJob==0
+			if self.cd_state==2 and self.currJob==2:
+				self.ab_state=0
+				self.currJob==0
+
+		self.lastRun = runFlag
+		return False,self.currJob 
+		
+							
+
 
 def syncPlc():
 	#=========================READ BIT===============================
@@ -189,7 +310,7 @@ def syncPlc():
 	for x in range(0, B1_CNT):
 		pinout[x] = plc01['B' + str(B1_OFFS + x)]
 	client.write_coils(START_ADDR + B1_OFFS,pinout,unit=UNIT_ADDR)
-	pinout = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	pinout = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	for x in range(0, B2_CNT):
 		pinout[x] = plc01['B' + str(B2_OFFS + x)]
 	client.write_coils(START_ADDR + B2_OFFS,pinout,unit=UNIT_ADDR)
@@ -206,10 +327,17 @@ def syncPlc():
 	client.write_register(4297, int(plc01['D201F']), unit=UNIT_ADDR)
 	return 0
 
+def alarm_event():
+	plc01['E01'] = plc01['S1']
+
 if __name__ == "__main__":
+	tb = table()
 	while True:
 		try:
-			syncPlc()	
+			syncPlc()
+			alarm_event()
+			#cstart,curr_table = tb.double_table(plc01['program-is-run'])	
+						
 		except KeyboardInterrupt:
 			print("Connection was closed")
 			client.close()
